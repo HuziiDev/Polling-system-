6e39f03a9a358021b728bfe4ad5ededae5905583
HuziiDev <huzaifaasif842@gmail.com>
2025-07-30 12:07:14 +0530

    second

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://polling-backend-liart.vercel.app");
 
 function useUniqueStudentName() {
   const [name, setName] = useState(() => sessionStorage.getItem("studentName") || "");
